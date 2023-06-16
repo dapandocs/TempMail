e9c257371b3f728b088bb7c19a4741ef8d97ce27
sunpanyu <996251389@qq.com>
2023-06-16 15:40:57 +0800

    style: update layout.

---
+++ app/GlobalSetting.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Navigation from "@/components/Navigation";
+import { ConfigProvider } from "antd";
+
+export default function GlobalSetting({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="bg-gradient-to-t from-slate-900 to-slate-700 h-screen text-slate-100 font-mono">
+      <Navigation />
+      <div className="h-1 border-b border-b-slate-500/30 w-screen shadow-2xl mb-4 lg:mb-8" />
+      <div className="pr-2 pl-2 lg:pr-0 lg:pl-0">
+        <ConfigProvider
+          theme={{
+            token: {
+              colorBgContainer: "transparent",
+              colorText: "#fff",
+              colorTextHeading: "#fff",
+              colorBorderSecondary: "rgb(244 245 249 / 0.2)",
+              colorPrimaryHover: "#fff",
+              colorBorder: "rgb(244 245 249 / 0.2)",
+              colorPrimaryActive: "#fff",
+            },
+          }}
+        >
+          {children}
+        </ConfigProvider>
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import Navigation from "@/components/Navigation";
-import { ConfigProvider } from "antd";
+import GlobalSetting from "./globalSetting";
 import "./globals.css";
 
 export const metadata = {
@@ -18,27 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <div className="bg-gradient-to-t from-slate-900 to-slate-700 h-screen text-slate-100 font-mono">
-          <Navigation />
-          <div className="h-1 border-b border-b-slate-500/30 w-screen shadow-2xl mb-4 lg:mb-8" />
-          <div className="pr-2 pl-2 lg:pr-0 lg:pl-0">
-            <ConfigProvider
-              theme={{
-                token: {
-                  colorBgContainer: "transparent",
-                  colorText: "#fff",
-                  colorTextHeading: "#fff",
-                  colorBorderSecondary: "rgb(244 245 249 / 0.2)",
-                  colorPrimaryHover: "#fff",
-                  colorBorder: "rgb(244 245 249 / 0.2)",
-                  colorPrimaryActive: "#fff",
-                },
-              }}
-            >
-              {children}
-            </ConfigProvider>
-          </div>
-        </div>
+        <GlobalSetting>{children}</GlobalSetting>
       </body>
     </html>
   );
